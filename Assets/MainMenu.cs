4f3490e56d756d9a81ac80ac2540a9cedf295827
ECR874 <educramos2007@gmail.com>
2025-10-24 22:31:16 -0300

    k

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject PainelMenuPrincipal;
+    [SerializeField] private SpriteRenderer Player;
+    [SerializeField] private TextMeshProUGUI Contador;
+    [SerializeField] private TextMeshProUGUI PlayButton;
+    private float DefaultColor = 1f;
+
+    public void Start()
+    {
+        PainelMenuPrincipal.SetActive(true);
+        Player.color = new Color(DefaultColor, DefaultColor, DefaultColor, 0f);
+        Contador.color = new Color(DefaultColor, DefaultColor, DefaultColor, 0f);
+        Time.timeScale = 0;
+    }
+
+    public void Play()
+    {
+        PainelMenuPrincipal.SetActive(false);
+        Player.color = new Color(DefaultColor, DefaultColor, DefaultColor, 1f);
+        Contador.color = new Color(DefaultColor, DefaultColor, DefaultColor, 1f);
+        Time.timeScale = 1;
+    }
+}
